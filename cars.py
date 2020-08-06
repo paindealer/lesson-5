65ae1707e3bdc9f61de59baf6590cbb561806203
paindealer <63595767+paindealer@users.noreply.github.com>
2020-08-06 18:34:49 +0300

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,16 @@
+class car:
+    def __init__(self, color, age):
+        self.color = color
+        self.age = age
+
+class diesel_car:
+    def __init__(self, fuel, color, age):
+        self.diesel = fuel
+        self.color = color
+        self.age = age
+
+class electric_car:
+    def __init__(self, fuel, color, age ):
+        self.electric = fuel
+        self.color = color
+        self.age = age
